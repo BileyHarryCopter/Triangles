fba62f74b62b98b4cf8cd021dc02eac6a2240b94
KetchuppOfficial <dolgov04@list.ru>
2022-09-28 18:49:29 +0300

    Simple build system and a small test for vectors added

---
+++ test/test_vector.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include "vector.hpp"
+
+using namespace Geom_Objects;
+
+TEST(Vector_Comparison, first)
+{
+    Vector vec_1 {1.0, 2.0, 3.0};
+    Vector vec_2 {-1.0, 2.0, 3.0};
+    
+    EXPECT_EQ (are_equal (vec_1, vec_2), false);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
