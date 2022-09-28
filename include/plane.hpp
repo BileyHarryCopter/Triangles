82b60a9703769e6dba4313bfe03af35fd0867127
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-09-28 22:40:07 +0300

    Implementation of header function to src directory

--- include/plane.hpp
+++ include/plane.hpp
@@ -36,66 +36,29 @@ public:
 };
 
 //  block with Plane and Point
-double distance (const Point& p, const Plane& pl)
-{
-    Vector vec  = {p.x_ - pl.x_, p.y_ - pl.y_, p.z_ - pl.z_};
-    double coef = scalar_product (vec, pl.norm_vec()); 
-    Vector prc  = vec * coef;
-    return prv.module();
-}
+double distance (const Point& p, const Plane& pl);
+double distance (const Plane& pl, const Point& p);
 
-double distance (const Plane& pl, const Point& p) { return distance (p, pl); }
-
-bool is_belong (const Point& p, const Plane& pl) { return Comparison::are_equal (distance (p, pl), 0.0); }
+bool is_belong (const Point& p, const Plane& pl);
 
 //  block with Plane and Line
-bool is_intersect (const Line& line, const Plane& pl) {
-    return Comparison::are_equal (scalar_product (pl.norm_vec(), line.drc_vec_),0.0); 
-}
+bool is_intersect (const Line& line, const Plane& pl);
+bool are_paral (const Plane& pl, const Line& line);
+bool are_paral (const Line& line, const Plane& pl);
 
-bool are_paral (const Plane& pl, const Line& line) { return are_collinear(pl.norm_vec(), line.drc_vec_); }
-bool are_paral (const Line& line, const Plane& pl) { return are_paral(pl, line); }
+bool is_consisted (const Line& line, const Plane& pl);
 
-bool is_consisted (const Line& line, const Plane& pl) 
-{
-    return Comparison::are_equal(pl.A_*line.point_.x_ + pl.B_*line.point_.y_ + pl.C_*line.point_.z_ + pl.D_, 0.0) 
-        && Comparison::are_equal(pl.A_*line.drc_vec_.x_ + pl.B_*line.drc_vec_.y_ + pl.C_*line.drc_vec_.z_, 0.0);
-}
+Point p_intersect (const Line& line, const Plane& pl);
 
-Point p_intersect (const Line& line, const Plane& pl)
-{
-    if (!is_intersect (line, pl))
-        return Point p = {nan, nan, nan};
-    //  from radius-vector form of line: r = r_{0} + drc_vec_*t
-    double t = - (pl.A_*line.point_.x_ + pl.B_*line.point_.y_ + pl.C_*line.point_.z_ + pl.D_) / 
-                 (pl.A_*line.drc_vec_.x_ + pl.B_*line.drc_vec_.y_ + pl.C_*line.drc_vec_.z_);
-    return Point p_inrs = { line.point_.x_ + line.drc_vec_.x_*t
-                          + line.point_.y_ + line.drc_vec_.y_*t
-                          + line.point_.z_ + line.drc_vec_.z_*t};
-}
-
-double distance (const Plane& pl, const Line& line)
-{
-    if (is_consisted(pl, line))
-        return 0.0;
-    return vector_product (pl.norm_vec(), line.drc_vec_).module();
-}
-double distance (const Line& line, const Plane& pl) { return distance(pl, line); }
+double distance (const Plane& pl, const Line& line);
+double distance (const Line& line, const Plane& pl);
 
 //  block with Plane and Plane
-bool are_paral (const Plane& pl1, const Plane& pl2) {
-    return Comparison::are_equal (scalar_product(pl1.norm_vec(), pl2.norm_vec()), 0.0);
-}
-bool are_intersect (const Plane& pl1, const Plane& pl2) { return !are_paral(pl1, pl2) }\
-bool are_equal (const Plane& pl1, const Plane& pl2) { 
-    return are_paral(pl1, pl2) && Comparison::are_equal (pl1.D_, pl2.D_); 
-}
+bool are_paral (const Plane& pl1, const Plane& pl2);
 
-double distance (const Plane& pl1, const Plane& pl2)
-{
-    if (are_equal(pl1, pl2))
-        return 0.0;
-    return std::abs(pl1.D_ - pl2.D_); 
-}
+bool are_intersect (const Plane& pl1, const Plane& pl2);
+bool are_equal (const Plane& pl1, const Plane& pl2);
+
+double distance (const Plane& pl1, const Plane& pl2);
 
 }   //  namespace Geom_Objects
