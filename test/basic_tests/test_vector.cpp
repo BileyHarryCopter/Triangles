b0321a9c685873fd1a1455af4c6e7bb730b8c233
KetchuppOfficial <dolgov04@list.ru>
2022-09-30 17:48:53 +0300

    Overloaded operator used; more suitable test-macro used

--- test/basic_tests/test_vector.cpp
+++ test/basic_tests/test_vector.cpp
@@ -8,11 +8,5 @@ TEST(Vector_Comparison, first)
     Vector vec_1 {1.0, 2.0, 3.0};
     Vector vec_2 {-1.0, 2.0, 3.0};
     
-    EXPECT_EQ (are_equal (vec_1, vec_2), false);
-}
-
-int main(int argc, char **argv)
-{
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    EXPECT_TRUE (vec_1 != vec_2);
 }
