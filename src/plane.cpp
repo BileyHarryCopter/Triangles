5f2079e26b8987e7f359bd3235c1f9ebdb409a49
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-10-04 00:23:49 +0300

    Functions distance(pl, p) and is_belong(p, pl) are returned in the header back. Figure out why it happend

--- src/plane.cpp
+++ src/plane.cpp
@@ -4,18 +4,7 @@ namespace Geom_Objects
 {
 
 //  block with Plane and Point
-double distance (const Point &p, const Plane &pl)
-{
-    Point origin = pl.origin ();
-    Vector vec   = {p.x_ - origin.x_, p.y_ - origin.y_, p.z_ - origin.z_};
-    double coef  = scalar_product (vec, pl.norm_vec ());
-    vec *= coef;
-    return vec.module ();
-}
-
-double distance (const Plane &pl, const Point &p) { return distance (p, pl); }
-
-bool is_belong (const Point &p, const Plane &pl) { return cmp::are_equal (distance (p, pl), 0.0); }
+//  Why EMPTY?
 
 //  block with Plane and Line
 bool is_intersect (const Line &line, const Plane &pl)
