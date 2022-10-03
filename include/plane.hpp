30e438abac820c9dc10410bae9d4fedcb127f5f7
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-10-03 21:00:34 +0300

    New realisation of plane with constructor from point and vector

--- include/plane.hpp
+++ include/plane.hpp
@@ -10,30 +10,42 @@ namespace Geom_Objects
 
 class Plane
 {
-    //  from general form of plane: Ax + By + Cz + D = 0
-    double A_, B_, C_, D_;
+    //  By an origin and normal vector of the plane
+    Vector normal_;
+    Point origin_;
 
 public:
 
-    Plane (double A, double B, double C, double D) : A_ {A}, B_ {B}, C_ {C}, D_ {D}
+    Plane (Vector &normal, Point &origin)
     {
-        //  three comparisons to not create unnecessery evict as vector
-        if (Comparison::are_equal (A, 0.0) && Comparison::are_equal (B, 0.0) &&
-            Comparison::are_equal (C, 0.0))
-            throw std::invalid_argument {
-                "In constructor of Plane(A, B, C, D): zero normal vector\n"};
+        if (normal.is_zero ())
+            throw std::invalid_argument {"Zero vector was given to constructor of Plane\n"};
+        normal_ = normal * (1 / normal.module ());
+        origin_ = origin;
     }
 
-    Plane (const Point &p, const Vector &rhs)
-        : A_ {rhs.x_}, B_ {rhs.y_}, C_ {rhs.z_}, D_ {p.x_ * rhs.x_ + p.y_ * rhs.y_ + p.z_ * rhs.z_}
+    //  Bad idea to construct a plane by 4 doubles
+    Plane (double A, double B, double C, double D)
     {
-        if (rhs.is_zero ())
-            throw std::invalid_argument {
-                "In constuctor of Plane(Vector normal, Point origin): zero normal\n"};
+        //  Three comparisons to not create unnecessery evict as vector
+        if (normal_.is_zero ())
+            throw std::invalid_argument {"Zero vector was given to constructor of Plane\n"};
+        if (cmp::are_equal (A, 0.0))
+        {
+            if (cmp::are_equal (B, 0.0))
+                origin_ = {0, 0, -D / C};
+            else
+                origin_ = {0, -D / B, 0};
+        }
+        else
+            origin_ = {-D / A, 0, 0};
+
+        normal_ = {A, B, C};
+        normal_ *= (1 / normal_.module ());
     }
 
-    Vector norm_vec () const { return Vector norm = {A_, B_, C_}; };
-    Point origin () const { return Point or = {p.x_ * rhs.x_, p.y_ * rhs.y_, p.z_ * rhs.z_}; }
+    Point origin () const { return Point {origin_.x_, origin_.y_, origin_.z_}; };
+    Vector norm_vec () const { return Vector {normal_.x_, normal_.y_, normal_.z_}; };
 };
 
 //  block with Plane and Point
@@ -44,18 +56,16 @@ bool is_belong (const Point &p, const Plane &pl);
 
 //  block with Plane and Line
 bool is_intersect (const Line &line, const Plane &pl);
-bool are_paral (const Plane &pl, const Line &line);
-bool are_paral (const Line &line, const Plane &pl);
+bool are_parallel (const Plane &pl, const Line &line);
+bool are_parallel (const Line &line, const Plane &pl);
 
-bool is_consisted (const Line &line, const Plane &pl);
-
-Point p_intersect (const Line &line, const Plane &pl);
+bool is_contained (const Line &line, const Plane &pl);
 
 double distance (const Plane &pl, const Line &line);
 double distance (const Line &line, const Plane &pl);
 
 //  block with Plane and Plane
-bool are_paral (const Plane &pl1, const Plane &pl2);
+bool are_parallel (const Plane &pl1, const Plane &pl2);
 
 bool are_intersect (const Plane &pl1, const Plane &pl2);
 bool are_equal (const Plane &pl1, const Plane &pl2);
--- src/plane.cpp
+++ src/plane.cpp
@@ -1,4 +1,4 @@
-#include "palne.hpp"
+#include "plane.hpp"
 
 namespace Geom_Objects
 {
@@ -6,75 +6,62 @@ namespace Geom_Objects
 //  block with Plane and Point
 double distance (const Point &p, const Plane &pl)
 {
-    Vector vec  = {p.x_ - pl.x_, p.y_ - pl.y_, p.z_ - pl.z_};
-    double coef = scalar_product (vec, pl.norm_vec ());
-    Vector prc  = vec * coef;
-    return prv.module ();
+    Point origin = pl.origin ();
+    Vector vec   = {p.x_ - origin.x_, p.y_ - origin.y_, p.z_ - origin.z_};
+    double coef  = scalar_product (vec, pl.norm_vec ());
+    vec *= coef;
+    return vec.module ();
 }
 
 double distance (const Plane &pl, const Point &p) { return distance (p, pl); }
 
-bool is_belong (const Point &p, const Plane &pl)
-{
-    return Comparison::are_equal (distance (p, pl), 0.0);
-}
+bool is_belong (const Point &p, const Plane &pl) { return cmp::are_equal (distance (p, pl), 0.0); }
 
 //  block with Plane and Line
 bool is_intersect (const Line &line, const Plane &pl)
 {
-    return Comparison::are_equal (scalar_product (pl.norm_vec (), line.drc_vec_), 0.0);
+    return cmp::are_equal (scalar_product (pl.norm_vec (), line.drc_vec ()), 0.0);
 }
 
-bool are_paral (const Plane &pl, const Line &line)
+bool are_parallel (const Plane &pl, const Line &line)
 {
-    return are_collinear (pl.norm_vec (), line.drc_vec_);
+    return are_collinear (pl.norm_vec (), line.drc_vec ());
 }
-bool are_paral (const Line &line, const Plane &pl) { return are_paral (pl, line); }
+bool are_parallel (const Line &line, const Plane &pl) { return are_parallel (pl, line); }
 
 bool is_consisted (const Line &line, const Plane &pl)
 {
-    return Comparison::are_equal (pl.A_ * line.point_.x_ + pl.B_ * line.point_.y_ +
-                                      pl.C_ * line.point_.z_ + pl.D_,
-                                  0.0) &&
-           Comparison::are_equal (
-               pl.A_ * line.drc_vec_.x_ + pl.B_ * line.drc_vec_.y_ + pl.C_ * line.drc_vec_.z_, 0.0);
+    return is_belong (line.point_, pl) && are_parallel (line, pl);
 }
 
-Point p_intersect (const Line &line, const Plane &pl)
-{
-    if (!is_intersect (line, pl))
-        return Point p = {nan, nan, nan};
-    //  from radius-vector form of line: r = r_{0} + drc_vec_*t
-    double t = -(pl.A_ * line.point_.x_ + pl.B_ * line.point_.y_ + pl.C_ * line.point_.z_ + pl.D_) /
-               (pl.A_ * line.drc_vec_.x_ + pl.B_ * line.drc_vec_.y_ + pl.C_ * line.drc_vec_.z_);
-    return Point p_inrs = {line.point_.x_ + line.drc_vec_.x_ * t + line.point_.y_ +
-                           line.drc_vec_.y_ * t + line.point_.z_ + line.drc_vec_.z_ * t};
-}
 
 double distance (const Plane &pl, const Line &line)
 {
-    if (is_consisted (pl, line))
+    if (is_consisted (line, pl))
         return 0.0;
-    return vector_product (pl.norm_vec (), line.drc_vec_).module ();
+    return vector_product (pl.norm_vec (), line.drc_vec ()).module ();
 }
 double distance (const Line &line, const Plane &pl) { return distance (pl, line); }
 
 //  block with Plane and Plane
-bool are_paral (const Plane &pl1, const Plane &pl2)
+bool are_parallel (const Plane &pl1, const Plane &pl2)
 {
-    return Comparison::are_equal (scalar_product (pl1.norm_vec (), pl2.norm_vec ()), 0.0);
+    return are_collinear (pl1.norm_vec (), pl2.norm_vec ());
 }
-bool are_intersect (const Plane &pl1, const Plane &pl2) { return !are_paral (pl1, pl2) }
+bool are_intersect (const Plane &pl1, const Plane &pl2) { return !are_parallel (pl1, pl2); }
 bool are_equal (const Plane &pl1, const Plane &pl2)
 {
-    return are_paral (pl1, pl2) && Comparison::are_equal (pl1.D_, pl2.D_);
+    return are_parallel (pl1, pl2) && is_belong (pl1.origin (), pl2);
 }
 
 double distance (const Plane &pl1, const Plane &pl2)
 {
+    if (!are_parallel (pl1, pl2))
+        throw std::invalid_argument {"Zero vector was given to constructor of Plane\n"};
     if (are_equal (pl1, pl2))
         return 0.0;
-    return std::abs (pl1.D_ - pl2.D_);
+    Vector diff {pl2.origin (), pl1.origin ()};
+    return std::abs (scalar_product (pl1.norm_vec (), diff));
 }
 
 } // namespace Geom_Objects
