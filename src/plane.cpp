b3d3d320249304188455bc745e0f1f390fed342b
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-10-04 14:25:05 +0300

    Empty src/plane.cpp. Just at the moment I hope

--- src/plane.cpp
+++ src/plane.cpp
@@ -4,53 +4,12 @@ namespace Geom_Objects
 {
 
 //  block with Plane and Point
-//  Why EMPTY?
+//  EMPTY
 
 //  block with Plane and Line
-bool is_intersect (const Line &line, const Plane &pl)
-{
-    return cmp::are_equal (scalar_product (pl.norm_vec (), line.drc_vec ()), 0.0);
-}
-
-bool are_parallel (const Plane &pl, const Line &line)
-{
-    return are_collinear (pl.norm_vec (), line.drc_vec ());
-}
-bool are_parallel (const Line &line, const Plane &pl) { return are_parallel (pl, line); }
-
-bool is_consisted (const Line &line, const Plane &pl)
-{
-    return is_belong (line.point_, pl) && are_parallel (line, pl);
-}
-
-
-double distance (const Plane &pl, const Line &line)
-{
-    if (is_consisted (line, pl))
-        return 0.0;
-    return vector_product (pl.norm_vec (), line.drc_vec ()).module ();
-}
-double distance (const Line &line, const Plane &pl) { return distance (pl, line); }
+//  EMPTY
 
 //  block with Plane and Plane
-bool are_parallel (const Plane &pl1, const Plane &pl2)
-{
-    return are_collinear (pl1.norm_vec (), pl2.norm_vec ());
-}
-bool are_intersect (const Plane &pl1, const Plane &pl2) { return !are_parallel (pl1, pl2); }
-bool are_equal (const Plane &pl1, const Plane &pl2)
-{
-    return are_parallel (pl1, pl2) && is_belong (pl1.origin (), pl2);
-}
-
-double distance (const Plane &pl1, const Plane &pl2)
-{
-    if (!are_parallel (pl1, pl2))
-        throw std::invalid_argument {"Zero vector was given to constructor of Plane\n"};
-    if (are_equal (pl1, pl2))
-        return 0.0;
-    Vector diff {pl1.origin (), pl2.origin ()};
-    return std::abs (scalar_product (pl1.norm_vec (), diff));
-}
+//  EMPTY
 
 } // namespace Geom_Objects
